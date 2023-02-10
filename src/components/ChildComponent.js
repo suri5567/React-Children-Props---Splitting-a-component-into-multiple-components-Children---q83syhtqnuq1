c7f5746d253e73e9f6dd07d710939caab0b22c30
Surinder kumar <117210849+suri5567@users.noreply.github.com>
2023-02-10 12:01:11 +0530

    Update ChildComponent.js

--- src/components/ChildComponent.js
+++ src/components/ChildComponent.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const ChildComponent = () => {
+const ChildComponent = (props) => {
 //code here
+const update = ()=>{
+  props.result*result   
+}
   return (
     <div id="child">
       Find The Square <br/>
-     
+        update;     
     </div>
   )
 }
